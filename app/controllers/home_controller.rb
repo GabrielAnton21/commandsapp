84dce3a1ab8c3314e85ea7860142ebb1ebc0e90e
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2021-10-19 05:59:13 -0400

    Created navbar, started about/donation added routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+
+  end
+  def desprenoi
+  end
+  def contact
+  end
+  def evenimente
+  end
+  def scoalaromana
+  end
+  def donatii 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #get 'home/index'
+  get 'home/desprenoi'
+  get 'home/donatii'
+  get 'home/scoalaromana'
+  get 'home/evenimente'
+  get 'home/contact'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
