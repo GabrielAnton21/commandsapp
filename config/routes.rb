2e4a2e93a29ddcf912fad7bf34528e56edf1430a
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2021-10-20 08:01:56 -0400

    Added slideshow, homepage, changed navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   #get 'home/index'
   get 'home/desprenoi'
+  get 'home/desprenoi/misiuni'
+  get 'home/desprenoi/regulamente'
+  get 'home/desprenoi/consiliul'
+  get 'home/desprenoi/inscriere'
+
   get 'home/donatii'
   get 'home/scoalaromana'
+
   get 'home/evenimente'
+
   get 'home/contact'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
